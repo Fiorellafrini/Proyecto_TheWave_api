f7a21602636c0655fbc18fb7d17f1de031af8875
henrydjpacheco <henrydjpacheco@gmailcom>
2023-04-11 16:06:00 -0500

    relaciones y modelos

--- api/src/app.js
+++ api/src/app.js
@@ -5,9 +5,6 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const passport = require('passport')
 
-
-require('./db.js');
-
 require("./db.js");
 
 const app = express();
@@ -19,14 +16,14 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
 })
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
@@ -35,10 +32,10 @@ app.use("/", routes);
 // Error catching endware.
 app.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
 });
 
 module.exports = app;
