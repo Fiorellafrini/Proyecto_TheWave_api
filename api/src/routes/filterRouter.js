588d81294ccbe632ce5ddf2bce30576ef7c8d63a
fiorella frini <fiorella_frini@hotmail.com>
2023-04-14 01:13:02 -0300

    filterType okk

--- api/src/routes/filterRouter.js
+++ api/src/routes/filterRouter.js
@@ -1,9 +1,6 @@
 const { Router } = require("express");
 const filterRouter = Router();
 const { Product, Type, Brand } = require("../db");
-const { Op } = require("sequelize");
-
-// // filterRouter.get("/brand", async (req, res) => {});
 
 filterRouter.get("/:type_id", async (req, res) => {
   try {
@@ -32,19 +29,5 @@ filterRouter.get("/brands/:brands_id", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
-
-// let info = require("../controllers/info");
-
-// filterRouter.get("/:typeName", async (req, res) => {
-//   try {
-//     console.log(info);
-//     const filteredData = info.filter(
-//       (product) => product.typeName === req.params.typeName
-//     );
-//     res.status(200).json(filteredData);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
 
 module.exports = filterRouter;
