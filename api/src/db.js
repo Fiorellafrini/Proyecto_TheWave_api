0a6df00a6faae9a989fb5531100da9721cd75ab4
fiorella frini <fiorella_frini@hotmail.com>
2023-04-19 15:24:11 -0300

    clou db con carga rapida

--- api/src/db.js
+++ api/src/db.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = process.env;
 
-// var sequelize = new Sequelize(
+// const sequelize = new Sequelize(
 //   `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
 //   {
 //     logging: false, // set to console.log to see the raw SQL queries
@@ -12,12 +12,11 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = process.e
 //   }
 // );
 
-var sequelize = new Sequelize( DB_DEPLOY, {
+const sequelize = new Sequelize( DB_DEPLOY, {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     }
   );
-
 
 const basename = path.basename(__filename);
 
