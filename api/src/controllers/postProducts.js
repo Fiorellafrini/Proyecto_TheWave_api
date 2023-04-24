e68a841d7b83f54327b7f5f04a2b00835a782bc5
federico <fedeasaad1099@outlook.com.ar>
2023-04-23 23:43:13 -0300

    ultimos cambios para la demo

--- api/src/controllers/postProducts.js
+++ api/src/controllers/postProducts.js
@@ -1,35 +1,33 @@
 const { Product, Type, Brand } = require("../db");
-const User = require("../db.js")
+const User = require("../db.js");
 
 const postProducts = async (product) => {
   try {
-
-    let { name, imagen, size, price, description,id_type, id_brand } = product;
-    if (!name && !imagen && !size && !price ) {
+    let { name, imagen, size, price, description, stock, id_type, id_brand } = product;
+    if (!name && !imagen && !size && !price) {
       throw new Error("Missing Information");
     }
     id_type = parseInt(id_type);
     id_brand = parseInt(id_brand);
-    
-      const newProduct = await Product.create({
+
+    const newProduct = await Product.create({
       name,
       imagen,
       size,
       price,
+      stock,
       description,
       id_type,
-      id_brand
+      id_brand,
     });
-    const types = await Type.findOne({where: {id_type: id_type}});
-    const brands = await Brand.findOne({where: {id_brand:id_brand}});
+    const types = await Type.findOne({ where: { id_type: id_type } });
+    const brands = await Brand.findOne({ where: { id_brand: id_brand } });
     newProduct.setType(types);
     newProduct.setBrand(brands);
-  return "exito"
-  }
-  catch (error) {
+    return "exito";
+  } catch (error) {
     return error.message;
   }
-
 };
 
 module.exports = postProducts;
--- api/src/routes/PaymentRouter.js
+++ api/src/routes/PaymentRouter.js
@@ -4,7 +4,6 @@ const mercadopago = require("mercadopago");
 
 paymentsRouter.post("/", (req, res) => {
   const products = Array.isArray(req.body) ? req.body : [req.body];
-
   let preference = {
     items: products.map((product) => ({
       id: product.id,
@@ -17,21 +16,17 @@ paymentsRouter.post("/", (req, res) => {
       unit_price: product.price,
     })),
     back_urls: {
-      success: "http://localhost:3000/SectionHome",
-      failure:
-        "https://proyecto-the-wave-client-1kip.vercel.app/SectionHome",
+      success: "https://proyecto-the-wave-client-1kip.vercel.app/SectionHome",
+      failure: "https://proyecto-the-wave-client-1kip.vercel.app/SectionHome",
       pending: "",
     },
     auto_return: "approved",
     binary_mode: true,
   };
-  
   mercadopago.preferences
     .create(preference)
     .then((response) => res.status(200).send({ response }))
     .catch((error) => res.status(400).send({ error: error.message }));
-  
 });
-
 
 module.exports = paymentsRouter;
