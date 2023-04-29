c8f8b6f00d7e554d44529affa5337c9b27aeaa30
Constanza <cotyespeche@hotmail.com>
2023-04-29 02:04:00 -0300

    sin where

--- api/src/controllers/activeUser.js
+++ api/src/controllers/activeUser.js
@@ -3,18 +3,19 @@ const { User } = require ("../db");
 
 
 const updateUserActive = async (id, active) => {
-    try {
-      const user = await User.findOne({ where: { id: id } });
-      if (!user) {
-        throw new Error("User not found");
-      }
-      await User.update({ active: active }, { where: { id: id } });
-      const updatedUser = await User.findOne({ where: { id: id } });
-      return updatedUser;
-    } catch (error) {
-      throw new Error(error.message);
+  try {
+    const user = await User.findOne({ where: { id: id } });
+    if (!user) {
+      throw new Error("User not found");
     }
-  };  
+    await User.update({ active: active }, { where: { id: id } });
+    const userList = await User.findAll();
+    return userList;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+ 
 
   
   
