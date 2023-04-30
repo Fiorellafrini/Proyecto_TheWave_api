425fc1db4a50bd67a2c7daab76cae521a861c258
fiorella frini <fiorella_frini@hotmail.com>
2023-04-30 18:00:57 -0300

    fav

--- api/src/controllers/fav.js
+++ api/src/controllers/fav.js
@@ -1,43 +1,38 @@
 const { Favorite, Product, User } = require("../db");
 
-
-
 async function addToFavorites(req, res) {
   const { userId, productId } = req.body;
 
   try {
     const user = await User.findByPk(userId);
 
-    const favorite = await Favorite.create({ 
+    const favorite = await Favorite.create({
       user_id: userId,
-      product_id: productId
+      product_id: productId,
     });
 
     res.status(200).json(favorite);
   } catch (err) {
     console.error(err);
-    res.status(400).json({ err:err.message });
+    res.status(400).json({ err: err.message });
   }
-  
 }
 
 //---------------DELETE FAV-----------------------------------------------------//
 async function removeFromFavorites(req, res) {
-    const { user_id, product_id } = req.params;
-    Favorite.destroy({ where: { user_id: user_id, product_id: product_id } })
-      .then(() => {
-        res.status(200).send({ product_id: product_id });
-      })
-      .catch((err) => {
-        res.status(400).send({ error: err.message });
-      });
-  };
-  
+  const { user_id, product_id } = req.params;
+  Favorite.destroy({ where: { user_id: user_id, product_id: product_id } })
+    .then(() => {
+      res.status(200).send({ product_id: product_id });
+    })
+    .catch((err) => {
+      res.status(400).send({ error: err.message });
+    });
+}
 
-  //-------------------TODOS LOS FAV DE UN USUARIO-----------------------------------//
-  
+//-------------------TODOS LOS FAV DE UN USUARIO-----------------------------------//
 
-async function getFavorites (req, res){
+async function getFavorites(req, res) {
   try {
     const { user_id } = req.params;
     const user = await User.findByPk(user_id);
@@ -46,13 +41,12 @@ async function getFavorites (req, res){
     // }
     const favorites = await Favorite.findAll({
       where: { user_id: user_id },
-      include: [{ model: Product }]
+      include: [{ model: Product }],
     });
     return res.json(favorites);
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
-};
+}
 
-
-  module.exports = { addToFavorites, removeFromFavorites, getFavorites }
+module.exports = { addToFavorites, removeFromFavorites, getFavorites };
