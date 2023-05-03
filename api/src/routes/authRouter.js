8f88781a15d1f4d9401fee4eaeab57d89a9f507a
fiorella frini <fiorella_frini@hotmail.com>
2023-05-03 10:30:19 -0300

    arreglo

--- api/src/routes/authRouter.js
+++ api/src/routes/authRouter.js
@@ -60,7 +60,7 @@ router.get("/google/callback",passport.authenticate("google", { failureRedirect:
       <body>
       </body>
       <script>
-       window.opener.postMessage(${tokenStr}, 'http://https://proyecto-the-wave-client-1kip.vercel.app')
+       window.opener.postMessage(${tokenStr}, 'https://proyecto-the-wave-client-1kip.vercel.app')
 
       </script>
     </html>`),
